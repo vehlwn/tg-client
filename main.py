46e7e0f9d1c36ad898df8362cb30b9434cc3e184
Ivan Bogdanov <c3t3o7@gmail.com>
2020-11-06 20:56:49 +0400

    First

---
+++ main.py
@@ -0,0 +1,125 @@
+import socks
+import configparser
+import argparse
+import telethon
+
+config = configparser.ConfigParser()
+config.read("api_id.ini")
+
+API_ID = config["general"]["api_id"]
+API_HASH = config["general"]["api_hash"]
+USERNAME = config["general"]["username"]
+
+TOR_PORT = 9050
+proxy = (socks.SOCKS5, "localhost", TOR_PORT)
+client = telethon.TelegramClient(USERNAME, API_ID, API_HASH, proxy=None)
+
+
+async def print_me():
+    me = await client.get_me()
+    print(me.stringify())
+    print(me.username)
+    print(me.phone)
+    async for dialog in client.iter_dialogs():
+        print(dialog.name, "has id", dialog.id)
+
+
+async def _get_messages_arr(chat_id, limit, from_user):
+    ret = []
+    async for message in client.iter_messages(
+        chat_id, limit=limit, from_user=from_user
+    ):
+        ret.append(message)
+    return ret
+
+
+async def get_messages(chat_id, limit, from_user):
+    ret = await _get_messages_arr(chat_id, limit, from_user)
+    for message in ret:
+        print(message.date, message.id, message.from_id, message.text)
+    print("len =", len(ret))
+
+
+async def send_message(chat_id, text):
+    await client.send_message(chat_id, text)
+
+
+async def delete_messages(chat_id, limit, from_user):
+    messages = await _get_messages_arr(chat_id, limit, from_user)
+    await client.delete_messages(chat_id, [x.id for x in messages], revoke=True)
+
+
+async def get_located(lat, long):
+    result = await client(
+        telethon.functions.contacts.GetLocatedRequest(
+            geo_point=telethon.types.InputGeoPoint(lat=lat, long=long),
+            background=False,
+        )
+    )
+    print(result.stringify())
+
+    class PeerUser:
+        def __init__(self, user_id, distance):
+            self.user_id = user_id
+            self.distance = distance
+
+    peer_users = []
+    for x in result.updates[0].peers:
+        if isinstance(x, telethon.tl.types.PeerSelfLocated) or not isinstance(
+            x.peer, telethon.tl.types.PeerUser
+        ):
+            continue
+        peer_users.append(PeerUser(x.peer.user_id, x.distance))
+    peer_users.sort(key=lambda x: x.distance)
+    for x in peer_users:
+        user = next(u for u in result.users if u.id == x.user_id)
+        print(
+            ("@" + user.username) if user.username else "",
+            ": " + user.first_name if user.first_name else "",
+            user.last_name if user.last_name else "",
+            ": id = " + str(x.user_id),
+            ": ",
+            str(x.distance) + " m ",
+        )
+
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--print-me", action="store_true")
+    parser.add_argument("--get-messages", action="store_true")
+    parser.add_argument("--send-message", action="store_true")
+    parser.add_argument("--delete-messages", action="store_true")
+    parser.add_argument("--get-located", action="store_true")
+    parser.add_argument("--limit", default=None)
+    parser.add_argument("--from-user", default=None)
+    parser.add_argument("--chat-id", default="me")
+    parser.add_argument("--text", default="")
+    parser.add_argument("--lat", type=float)
+    parser.add_argument("--long", type=float)
+    args = parser.parse_args()
+    if args.chat_id != "me":
+        args.chat_id = int(args.chat_id)
+    if args.from_user and args.from_user != "me":
+        args.from_user = int(args.from_user)
+    if args.limit:
+        args.limit = int(args.limit)
+
+    if args.print_me:
+        await print_me()
+    elif args.get_messages:
+        print(f"{args.from_user=}")
+        print(f"{args.chat_id=}")
+        await get_messages(args.chat_id, args.limit, args.from_user)
+    elif args.send_message:
+        await send_message(args.chat_id, args.text)
+    elif args.delete_messages:
+        await delete_messages(args.chat_id, args.limit, args.from_user)
+    elif args.get_located:
+        print(f"{args.lat=}")
+        print(f"{args.long=}")
+        await get_located(args.lat, args.long)
+
+
+with client:
+    print("Client created")
+    client.loop.run_until_complete(main())
